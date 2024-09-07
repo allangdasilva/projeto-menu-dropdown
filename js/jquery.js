f67ba426586e17696c9e19bd5c45b9b591b14744
Allan Silva <allandasilva33@outlook.com>
2024-09-06 22:04:02 -0300

    Estilização do responsivo, ainda sem interatividade

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$('.h-menu-icon-click').on('click', function(){
+    $('.h-nav').toggleClass('h-nav-mobile')
+    $('.h-icons').toggleClass('h-icons-mobile')
+})
+$('.h-nav-ul-li-portfolio').on('click', function(){
+    
+})
