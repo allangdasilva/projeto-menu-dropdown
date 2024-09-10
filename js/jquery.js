0e55deccc059c9eaeccd34764f35dffa1ba687a0
Allan Silva <allandasilva33@outlook.com>
2024-09-10 01:43:45 -0300

    Estilização e interação do menu concluída

--- js/jquery.js
+++ js/jquery.js
@@ -4,14 +4,24 @@ $('.icon-click').on('click', function(){
     $('.icons').toggleClass('icons-active')
     $('.nav').toggleClass('nav-active')
     $('.icon-button').toggleClass('icon-button-active')
+    if($('.services').hasClass('services-active')){
+        $('.services').removeClass('services-active')
+        $('.li-services div i').removeClass('icon-active')
+    }
+    if ($('.portfolio').hasClass('portfolio-active')){
+        $('.portfolio').removeClass('portfolio-active')
+        $('.li-portfolio div i').removeClass('icon-active')
+    }
 })
 
 // ------ Services click -------
-$('.li-services').on('click', function(){
+$('.li-services div').on('click', function(){
     $('.services').toggleClass('services-active')
+    $('.li-services div i').toggleClass('icon-active')
 })
 
 // ------ Portfolio click -------
-$('.li-portfolio').on('click', function(){
+$('.li-portfolio div').on('click', function(){
     $('.portfolio').toggleClass('portfolio-active')
+    $('.li-portfolio div i').toggleClass('icon-active')
 })
